26a7965ef0438f31b161dd37f178a38fb86d4bfa
Burak KOSE <burakks41@gmail.com>
2017-06-08 17:18:21 +0200

    Add script for starting to work

---
+++ config/scripts/elmar-work.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+{
+xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1-1 --off &
+feh -F "/home/burak/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
+intellij-idea-ultimate-edition &
+firefox &
+slack &
+telegram &
+thunderbird &
+google-chrome-stable &
+urxvt &
+spotify &
+} &> /dev/null
