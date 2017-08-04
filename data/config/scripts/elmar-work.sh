dfa2d5eabddca1525449ce5faa0b0b76407b6a72
Burak KOSE <burakks41@gmail.com>
2017-08-04 09:48:26 +0200

    Make user more generic

--- data/config/scripts/elmar-work.sh
+++ data/config/scripts/elmar-work.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 {
 xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1-1 --off &
-feh -F "/home/burak/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
+feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
 intellij-idea-ultimate-edition &
 firefox &
 slack &
