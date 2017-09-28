30af7125ea19e1344849eeb3d684546e0d51c638
Burak KOSE <burakks41@gmail.com>
2017-09-28 16:56:14 +0200

    Fix bookmark script behaviour

--- data/config/rofi/bookmarks.py
+++ data/config/rofi/bookmarks.py
@@ -1,11 +1,15 @@
 from pybase import rofi
 import csv
+import os
 import subprocess
 
-with open('bookmarks.csv') as csvfile:
+csv_file = os.path.join(os.path.dirname(__file__), "bookmarks.csv")
+
+with open(csv_file) as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     attr = list(reader)[1:]
     (options, values) = zip(*attr)
     r = rofi.Rofi()
     index = r.select('Select bookmark: ', options)[0]
-    subprocess.call(['firefox', values[index]])
+    if index != -1:
+        subprocess.call(['firefox', values[index]])
