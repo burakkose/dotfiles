ad7942524b6e0b3d9c88e58d5dbfd45717c8058e
Burak KOSE <burakks41@gmail.com>
2017-09-28 16:03:43 +0200

    Add bookmarks selection to Rofi

---
+++ data/config/rofi/bookmarks.py
@@ -0,0 +1,11 @@
+from pybase import rofi
+import csv
+import subprocess
+
+with open('bookmarks.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    attr = list(reader)[1:]
+    (options, values) = zip(*attr)
+    r = rofi.Rofi()
+    index = r.select('Select bookmark', options)[0]
+    subprocess.call(['firefox', values[index]])
