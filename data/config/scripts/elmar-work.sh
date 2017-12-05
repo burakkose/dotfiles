64bfff48308ab2a69af17060c367772b298e855f
Burak KOSE <burakks41@gmail.com>
2017-12-05 14:45:42 +0100

    Update work script

--- data/config/scripts/elmar-work.sh
+++ data/config/scripts/elmar-work.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 {
-xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1-1 --off &
+xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x336 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 1920x0 --rotate left --output DP-2 --off --output DP-1-1 --off
 feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
 intellij-idea-ultimate-edition &
-firefox &
-slack &
-telegram &
+firefox-developer &
+telegram-desktop &
 thunderbird &
-google-chrome-stable &
-urxvt &
 spotify &
 } &> /dev/null
