5995aebc7c6cea03e3dcdb5c2b6b84bafd0ce7e1
Burak KOSE <burakks41@gmail.com>
2018-02-05 21:33:05 +0100

    Add home script

---
+++ data/config/scripts/home.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+{
+xrandr --output HDMI-2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --off --output DP-2 --off && feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
+intellij-idea-ultimate-edition &
+firefox-developer &
+telegram-desktop &
+thunderbird &
+spotify &
+} &> /dev/null
