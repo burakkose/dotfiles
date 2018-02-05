0fdfcb8d78d5db64cf4fe34536d038c9ceda3e4f
Burak KOSE <burakks41@gmail.com>
2018-02-05 21:30:10 +0100

    Run feh after xrandr

--- data/config/scripts/work.sh
+++ data/config/scripts/work.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 {
-xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x336 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 1920x0 --rotate left --output DP-2 --off --output DP-1-1 --off
-feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
+xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x336 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 1920x0 --rotate left --output DP-2 --off --output DP-1-1 --off && feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
 intellij-idea-ultimate-edition &
 firefox-developer &
 telegram-desktop &
 thunderbird &
 spotify &
+slack &
 } &> /dev/null
