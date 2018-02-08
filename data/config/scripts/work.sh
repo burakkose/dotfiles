3ed0658890771718aa7d9099969339737d79f67e
Burak KOSE <burakks41@gmail.com>
2018-02-08 15:03:04 +0100

    Update work script

--- data/config/scripts/work.sh
+++ data/config/scripts/work.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 {
-xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x336 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 1920x0 --rotate left --output DP-2 --off --output DP-1-1 --off && feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
+xrandr --output eDP-1 --off --output DP-1-2 --primary --mode 1920x1200 --pos 0x0 --rotate right --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 1920x1200 --pos 1200x344 --rotate normal
+feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
 intellij-idea-ultimate-edition &
 firefox-developer &
 telegram-desktop &
