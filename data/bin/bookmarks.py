57265aa348ccc857bf3b37948cd6537012a5685a
Burak KOSE <kosburak@amazon.com>
2018-10-11 16:04:42 +0100

    Reorganize executable programs

--- data/bin/bookmarks.py
+++ data/bin/bookmarks.py
@@ -1,15 +1,21 @@
-from pybase import rofi
+import sys
+import os
+
+home = os.getenv("HOME")
+sys.path.append(home + "/.lib/pybase")
+
+from rofi import Rofi
 import csv
 import os
 import subprocess
 
-csv_file = os.path.join(os.path.dirname(__file__), "bookmarks.csv")
+csv_file = os.path.join(os.path.dirname(__file__), home + "/.config/rofi/bookmarks.csv")
 
 with open(csv_file) as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     attr = list(reader)[1:]
     (options, values) = zip(*attr)
-    r = rofi.Rofi()
+    r = Rofi()
     index = r.select('Select bookmark: ', options)[0]
     if index != -1:
         subprocess.call(['firefox', values[index]])
