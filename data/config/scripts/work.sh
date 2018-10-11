0dfa20cc5d4f47151a14345901a0dae0568368f6
Burak KOSE <kosburak@amazon.com>
2018-10-11 11:50:00 +0100

    Update work script

--- data/config/scripts/work.sh
+++ data/config/scripts/work.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 {
-xrandr --output eDP-1 --off --output DP-1-2 --primary --mode 1920x1200 --pos 0x0 --rotate right --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 1920x1200 --pos 1200x344 --rotate normal
-feh -F "~/dotfiles/config/wallpaper/wallpaper.png" --bg-fill &
-intellij-idea-ultimate-edition &
-firefox-developer &
-telegram-desktop &
-thunderbird &
-spotify &
-slack &
+xrandr \
+--output eDP-1 \
+--off --output DP-1-2 \
+--mode 2560x1440 \
+--pos 2616x0 --rotate left --output HDMI-2 \
+--off --output HDMI-1 \
+--off --output DP-1 \
+--off --output DP-1-3 \
+--off --output DP-2 \
+--off --output DP-1-1 \
+--primary --mode 2560x1440 \
+--pos 0x0 --rotate normal \
+ && feh -F "$HOME/.config/wallpaper/wallpaper.png" --bg-fill \
+ && (intellij-idea-community > /dev/null 2>&1 &) \
+ && (firefox > /dev/null 2>&1 &) \
+ && (spotify > /dev/null 2>&1 &)\
+ && (amazon-chime > /dev/null 2>&1 &) \
+ && (amazon-outlook > /dev/null 2>&1 &)
 } &> /dev/null
