cf622d7ff69817d16c21fb3a8efc80ce3853b4b6
Burak Kose <burakkose@microsoft.com>
2022-12-21 16:05:18 -0800

    Add script to install vscode extensions

---
+++ data/config/vscode/install-vscode-extensions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Visual Studio Code :: Package list
+pkglist=(
+alphabotsec.vscode-eclipse-keybindings
+Equinusocio.vsc-material-theme
+equinusocio.vsc-material-theme-icons
+ms-vscode-remote.remote-containers
+ms-vscode-remote.remote-ssh
+ms-vscode-remote.remote-ssh-edit
+ms-vscode-remote.remote-wsl
+ms-vscode-remote.vscode-remote-extensionpack
+ms-vscode.cpptools-themes
+ms-vscode.remote-explorer
+PKief.material-icon-theme
+)
+
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
