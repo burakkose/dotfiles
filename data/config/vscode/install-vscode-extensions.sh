aa7579e7ea9915bbc3062f013582657365e2af6c
Burak Kose <burakks41@gmail.com>
2023-01-21 01:11:48 -0800

    Update vscode extensions

--- data/config/vscode/install-vscode-extensions.sh
+++ data/config/vscode/install-vscode-extensions.sh
@@ -3,8 +3,12 @@
 # Visual Studio Code :: Package list
 pkglist=(
 alphabotsec.vscode-eclipse-keybindings
+Equinusocio.vsc-community-material-theme
 Equinusocio.vsc-material-theme
 equinusocio.vsc-material-theme-icons
+ms-python.isort
+ms-python.python
+ms-python.vscode-pylance
 ms-vscode-remote.remote-containers
 ms-vscode-remote.remote-ssh
 ms-vscode-remote.remote-ssh-edit
@@ -13,6 +17,8 @@ ms-vscode-remote.vscode-remote-extensionpack
 ms-vscode.cpptools-themes
 ms-vscode.remote-explorer
 PKief.material-icon-theme
+redhat.ansible
+redhat.vscode-yaml
 )
 
 for i in ${pkglist[@]}; do
