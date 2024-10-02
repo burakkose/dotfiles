4e65eb4b485e31aecf80fa00f1e19e165a7155f7
Burak Kose <burakks41@gmail.com>
2024-10-01 19:38:28 -0700

    Update extensions list

--- data/config/vscode/install-vscode-extensions.sh
+++ data/config/vscode/install-vscode-extensions.sh
@@ -3,22 +3,30 @@
 # Visual Studio Code :: Package list
 pkglist=(
 alphabotsec.vscode-eclipse-keybindings
-Equinusocio.vsc-community-material-theme
-Equinusocio.vsc-material-theme
+christian-kohler.path-intellisense
+eamodio.gitlens
+ecmel.vscode-html-css
+equinusocio.vsc-material-theme
 equinusocio.vsc-material-theme-icons
+ms-python.debugpy
 ms-python.isort
 ms-python.python
 ms-python.vscode-pylance
 ms-vscode-remote.remote-containers
 ms-vscode-remote.remote-ssh
 ms-vscode-remote.remote-ssh-edit
-ms-vscode-remote.remote-wsl
 ms-vscode-remote.vscode-remote-extensionpack
+ms-vscode.cmake-tools
+ms-vscode.cpptools
+ms-vscode.cpptools-extension-pack
 ms-vscode.cpptools-themes
 ms-vscode.remote-explorer
-PKief.material-icon-theme
+ms-vscode.remote-server
+pkief.material-icon-theme
 redhat.ansible
 redhat.vscode-yaml
+twxs.cmake
+zainchen.json
 )
 
 for i in ${pkglist[@]}; do
